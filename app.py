27456b57f8d530cf835b3bcb9470579730aaef8e
kwonangela <kwonangelaa@gmail.com>
2023-02-06 11:45:24 -0500

    JSON showing on localhost

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+from peewee import *
+from playhouse.shortcuts import model_to_dict, dict_to_model
+
+db = PostgresqlDatabase('avatar', user='postgres', password='', host='localhost', port=5432)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Atla(BaseModel):
+    name = CharField()
+    nation = CharField()
+    is_bender = BooleanField(default=True)
+
+db.connect()
+db.drop_tables([Atla])
+db.create_tables([Atla])
+
+Atla(name='Aang', nation='Air', is_bender=True).save()
+Atla(name='Katara', nation='Water', is_bender=True).save()
+Atla(name='Sokka', nation='Water', is_bender=False).save()
+Atla(name='Toph', nation='Earth', is_bender=True).save()
+Atla(name='Zuko', nation='Fire', is_bender=True).save()
+
+app = Flask(__name__)
+
+@app.route('/character/', methods=['GET', 'POST'])    
+@app.route('/character/<id>', methods=['GET', 'PUT', 'DELETE'])    
+
+def endpoint(id=None):
+    if request.method == 'GET':
+        if id:
+            return model_to_dict(Atla.get(Atla.id == id))
+        else:
+            char_list = []
+            for char in Atla.select():
+                char_list.append(model_to_dict(char))
+            return char_list
+
+    if request.method == 'PUT':
+        body = request.get_json()
+        Atla.update(body).where(Atla.id == id).execute()
+        return (f"Character {str(id)} has been updated.")
+
+    if request.method == 'POST':
+        new_char = dict_to_model(Atla, request.get_json())
+        new_char.save()
+        return jsonify({"success": True})
+    
+    if request.method == 'DELETE':
+        Atla.delete().where(Atla.id == id).execute()
+        return (f"Character {str(id)} deleted.")
+
+app.run(port=5000, debug=True)
+
